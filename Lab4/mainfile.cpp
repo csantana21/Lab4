2922ccdb5f6c12ec7c8a36ca48e9d6348f3884ca
Carlos Santana <csantana@ryerson.ca>
2015-10-21 16:38:10 -0400

    added a line to the program

---
+++ Lab4/mainfile.cpp
@@ -0,0 +1,52 @@
+/*
+ * mainfile.cpp
+ *
+ *  Created on: Oct 14, 2015
+ *      Author: csantana
+ */
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Headerfile.h"
+using namespace std;
+
+struct patient {
+		string firstname;
+		string lastname;
+		char sex;
+		char bloodtype;
+		string organ;
+		int age;
+		int year;
+	};
+
+patient arr[100];
+
+int main (int argc, char *argv[]){
+
+	int record = 0;
+	int result;
+
+	for(;1;)
+	{
+		cout << "There are currently " << record << " patient(s) in the list.";
+
+			result = options();
+
+
+			if(result == 1){
+				record = record + 1;
+				cout << "Please enter the following (in order):" << endl;
+				//cout <<
+			}
+			if (result == 2){}
+
+			if (result == 3){
+				cout << "The program has ended.";
+				break;
+			}
+	}
+
+}
+
